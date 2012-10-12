dab25c557099c92402b807c844139b3a61e64542
wuzhifeng <zhifeng_wu@netcom-hk.com>
2012-10-12 11:37:09 +0800

    先弄一个简单的频道列表放在外部存储器方便调式可以随时修改，频道可以上下切换。

--- src/com/wzf/LiveDemo/LiveDemoActivity.java
+++ src/com/wzf/LiveDemo/LiveDemoActivity.java
@@ -33,14 +33,14 @@ public class LiveDemoActivity extends Activity implements
 	private SurfaceView mPreview;
 	private SurfaceHolder holder;
 	private String path;
-	private Integer currentid = 1;
-	//private String cctv13 = "http://v.iseeyoo.cn/video/2012/01/26/3863b5e6-4801-11e1-89a1-001e0bd5b3ca_041832_001.mp4";
-	private String cctv13 ="rtsp://58.241.134.33:554/301";//"rtsp://218.204.223.237:554/live/1/66251FC11353191F/e7ooqwcfbqjoo80j.sdp"; //
-	private String cctv6  ="http://112.111.143.137:80/30285NfWL5jwG------Y-5cWss.5l5m5mXHJkkJ0-XJ0k-5lH--05mwG------Y-51kr0Y5c5c0X--5N5mfWL5j5Nvf5j-5cL5cL5N5mvf5j5N.I.o5jRY-0Y5cg5c0X--5c-5N5m.I.o5j5NpoF5jXHJkkJ0-XJ00g5lH-5cXHJkkJ0-XJ00r5lH-H-5c-5N5mpoF5j5NxdoB5jSewTTu5w-XHkX00RH-0r5cXHJkX0J0RHJ0r5N5mxdoB5j5NK8z5jvBovIp5lRU5cdfdK5lk0HJkJ0RrJ0Xr5lH-5c.sQ.o5l5cds5lDCF5csA5l0RggRg-HH05cZK5lXHJkkJ0-XJ00H5N5mK8z5j/temp.flv?start=1344294000000&end=1344301200000&key=1344340908000";
-	private String cctv4 ="http://fjwxbiz.vsdn.tv380.com/playlive.php?5B63686E5D445830303030303036307C323931387C317C343030307C434354562D347C687474705B2F63686E5D5B74735D317C687474703A2F2F35382E32322E3130352E3132303A383030312F445830303030303036305F323931385B2F74735DVSDNSOOONERCOM00";
-	private String cctv3 ="http://fjwxbiz.vsdn.tv380.com/playlive.php?5B63686E5D445830303030303036307C323931377C317C343030307C434354562D337C687474705B2F63686E5D5B74735D317C687474703A2F2F35382E32322E3130352E3132303A383030312F445830303030303036305F323931375B2F74735DVSDNSOOONERCOM00";
-	private String cctv2 ="http://fjwxbiz.vsdn.tv380.com/playlive.php?5B63686E5D445830303030303036307C323931367C317C343030307C434354562D327C687474705B2F63686E5D5B74735D317C687474703A2F2F35382E32322E3130352E3132303A383030312F445830303030303036305F323931365B2F74735DVSDNSOOONERCOM00";
-	private String cctv1 ="http://fjwxbiz.vsdn.tv380.com/playlive.php?5B63686E5D445830303030303036307C323931357C317C343030307C434354562D317C687474705B2F63686E5D5B74735D317C687474703A2F2F35382E32322E3130352E3132303A383030312F445830303030303036305F323931355B2F74735DVSDNSOOONERCOM00";
+	private Integer currentid = 0;
+	//private String channel13 = "http://v.iseeyoo.cn/video/2012/01/26/3863b5e6-4801-11e1-89a1-001e0bd5b3ca_041832_001.mp4";
+	private String channel6 ="rtsp://58.241.134.33:554/301";//"rtsp://218.204.223.237:554/live/1/66251FC11353191F/e7ooqwcfbqjoo80j.sdp"; //
+	private String channel5  ="rtsp://116.199.127.68/guoji";
+	private String channel4 ="rtsp://116.199.127.68/huayu";
+	private String channel3 ="rtsp://116.199.127.68/bengang";
+	private String channel2 ="rtsp://116.199.127.68/fenghuang";
+	private String channel1 ="rtsp://116.199.127.68/gztv_sport";
 	private Bundle extras;
 	private static final String MEDIA = "media";
 	private static final int LOCAL_AUDIO = 1;
@@ -104,21 +104,12 @@ public class LiveDemoActivity extends Activity implements
 	                 */
 	                //path = "rtsp://58.241.134.33:554/11";
 
-	                	switch(pathid)
-	                	{
-	                	case 13:path =cctv13;break;
-	                	case 6:
-														path=readtxt("/mnt/sda/sda1/1.txt");
-														if((path == "")||(path==null))
-														{
-	                						path =cctv6;
-	                					}
-	                					break;
-	                	case 4:path =cctv4;break;
-	                	case 3:path =cctv3;break;
-	                	case 2:path =cctv2;break;
-	                	case 1:path =cctv1;break;
-	                	default:path = "http://fjwxbiz.vsdn.tv380.com/playlive.php?5B63686E5D445830303030303036307C323932357C317C343030307C434354562D31337C687474705B2F63686E5D5B74735D317C687474703A2F2F35382E32322E3130352E3132303A383030312F445830303030303036305F323932355B2F74735DVSDNSOOONERCOM00";break;
+
+				path=readtxt("/mnt/sdcard/LiveDemo/"+pathid);
+
+				if((path == "")||(path==null))
+				{
+	                		path =channel1;
 	                	}
 
 
@@ -202,7 +193,7 @@ public class LiveDemoActivity extends Activity implements
 	public void surfaceCreated(SurfaceHolder holder) {
 	    Log.d(TAG, "surfaceCreated called");
 	    //playVideo(extras.getInt(MEDIA));
-	    playVideo(5,13);
+	    playVideo(STREAM_VIDEO,currentid);
 	
 	
 	}
@@ -242,35 +233,46 @@ public class LiveDemoActivity extends Activity implements
 	    
 	}
 	
-	public void resetvideo()
+	public void resetvideo(int key)
 	{
 			    	//mMediaPlayer.stop();
 	    		    //releaseMediaPlayer();
 	    		    mMediaPlayer.reset();
-	    		    if(13==currentid)
-	    		       	currentid=1;	    		    	
-	    		    else if(1==currentid)
-	    		       	currentid=2;
-	    		    else if(2==currentid)
-	    		       	currentid=3;
-	    		    else if(3==currentid)
-	    		       	currentid=4;
-	    		    else if(4==currentid)
-	    		       	currentid=6;
-	    		    else 
-	    		       	currentid=13;
-	    		    //playVideo(5,currentid);	
-	    		    
-	    		    switch(currentid)
-	                	{
-	                	case 13:path =cctv13;break;
-	                	case 6:path =cctv6;break;
-	                	case 4:path =cctv4;break;
-	                	case 3:path =cctv3;break;
-	                	case 2:path =cctv2;break;
-	                	case 1:path =cctv1;break;
-	                	default:path = "http://fjwxbiz.vsdn.tv380.com/playlive.php?5B63686E5D445830303030303036307C323932357C317C343030307C434354562D31337C687474705B2F63686E5D5B74735D317C687474703A2F2F35382E32322E3130352E3132303A383030312F445830303030303036305F323932355B2F74735DVSDNSOOONERCOM00";break;
-	                	}
+		int maxid=0;
+		try{ 
+			maxid=Integer.parseInt(readtxt("/mnt/sdcard/LiveDemo/chnum.txt")); 
+			Log.d(TAG, "maxid==" + maxid);
+		} 
+		catch(NumberFormatException   e) 
+		{ 
+			
+		}
+
+		if(1==key)
+		{
+			Log.d(TAG, "+++++++++++++");
+			currentid=++currentid%maxid;
+			Log.d(TAG, "currentid1==" + currentid);
+			if(maxid==currentid)
+				currentid=0;
+			Log.d(TAG, "currentid2==" + currentid);
+		}
+		else if(0==key) 
+		{	
+			Log.d(TAG, "------------");
+			if(0==currentid)
+				currentid=maxid;
+			currentid=--currentid%maxid;	
+		}
+	                	path=readtxt("/mnt/sdcard/LiveDemo/"+currentid);
+
+				Log.d(TAG, "path==================" + path);
+				if((path == "")||(path==null))
+				{
+							path =channel1;
+							Log.d(TAG, "update path==================" + path);
+                		}
+				
 	                try
 	                {
 		                mMediaPlayer.setDataSource(path);
@@ -291,22 +293,25 @@ public class LiveDemoActivity extends Activity implements
 	    			}
 	}
 	public boolean onKeyDown(int keyCode, KeyEvent msg) {
-
-			if((keyCode == KeyEvent.KEYCODE_MEDIA_NEXT)||(keyCode == KeyEvent.KEYCODE_DPAD_DOWN )||(keyCode == KeyEvent.KEYCODE_DPAD_RIGHT ))
-			{
-				resetvideo();
-	    }
-	    else if((keyCode == KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE)||(keyCode == KeyEvent.KEYCODE_DPAD_CENTER ))
-	    {
-	    	if(mMediaPlayer.isPlaying())
-	    		mMediaPlayer.pause();
-	    	else
-	    		mMediaPlayer.start();
-	    }
-	    else
-	        	return super.onKeyDown(keyCode, msg);
-    	return (true);
-    }
+		if((keyCode == KeyEvent.KEYCODE_MEDIA_NEXT)||(keyCode == KeyEvent.KEYCODE_DPAD_DOWN )||(keyCode == KeyEvent.KEYCODE_DPAD_RIGHT ))
+		{
+			resetvideo(1);
+		}
+		else if((keyCode == KeyEvent.KEYCODE_MEDIA_PREVIOUS)||(keyCode == KeyEvent.KEYCODE_DPAD_UP )||(keyCode == KeyEvent.KEYCODE_DPAD_LEFT ))
+		{
+			resetvideo(0);
+		}
+		else if((keyCode == KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE)||(keyCode == KeyEvent.KEYCODE_DPAD_CENTER ))
+		{
+	    		if(mMediaPlayer.isPlaying())
+	    			mMediaPlayer.pause();
+	    		else
+	    			mMediaPlayer.start();
+		}
+		else
+			return super.onKeyDown(keyCode, msg);
+    		return (true);
+	}
     
   public String readtxt(String filename) {  
   	String lineTXT = null;   
